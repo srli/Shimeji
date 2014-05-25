65f2c0147c702f113d50289ece9ef05ed4c93cf4
Sophia Li <sophia.li@students.olin.edu>
2014-05-25 04:28:18 -0400

    ok cairo is hard and i'm sleepy

--- ShimejiScreenlet.py
+++ ShimejiScreenlet.py
@@ -9,9 +9,11 @@ Created on Sat May 24 21:39:41 2014
 import screenlets
 import cairo
 from screenlets.options import BoolOption
-from time import sleep
+from time import time
 from random import randint
 import gettext
+
+i = 0
 
 _ = screenlets.utils.get_translator(__file__)
 
@@ -23,6 +25,7 @@ class ShimejiScreenlet (screenlets.Screenlet):
     __desc__ = __doc__
     
     def __init__ (self, **keyword_args):
+#        self.last_draw_time = time()
         #calls super, doesn't show window yet
         screenlets.Screenlet.__init__(self, uses_theme=True, **keyword_args)
         # sets theme
@@ -38,8 +41,8 @@ class ShimejiScreenlet (screenlets.Screenlet):
         self.update_shape()
         #show window
         self.window.show()
-        
-        self.add_options_group(_('Options'), _('Options'))
+#        self.last_draw_time = self.last_draw_time
+        self.add_options_group(_('Options'), _('Options'))       
     
     def __setattr__(self, name, value):
         #call Screenlet.__setattr___ in baseclass (Important!)
@@ -52,17 +55,23 @@ class ShimejiScreenlet (screenlets.Screenlet):
 
     
     def on_draw(self, ctx):
-        list_possible = ["shime1", "shime1a", "shime1b", "shime7", "shime9", "shime20", "shime30", "shime11"]
+        global i
+#        list_possible = ["shime1", "shime1a", "shime1b", "shime7", "shime9", "shime20", "shime30", "shime11"]
+        idle_list_forwards = ["shime1", "shime1a", "shime1b", "shime2", "shime3"]
+        idle_list_backwards = idle_list_forwards[::-1]
+        idle_positions = idle_list_forwards + idle_list_backwards        
         ctx.set_operator(cairo.OPERATOR_CLEAR)
         ctx.fill()
         ctx.set_operator(cairo.OPERATOR_OVER)
         ctx.scale(self.scale, self.scale)
-        if self.theme:
-            random_number = randint(0,7)
-            print "random number is ", random_number
-            self.theme.render(ctx, list_possible[random_number])
+        if i == 9:
+            i = 0
+        if self.theme: #or (self.last_draw_time - time() > 0.5):
+            print "idle_position ", i
+            self.theme.render(ctx, idle_positions[i])
             print "drawing now"
-    
+            i += 1
+                
     def on_draw_shape(self, ctx):
         #simple call drawing handler and pass shape-context
         self.on_draw(ctx)
@@ -76,17 +85,15 @@ class ShimejiScreenlet (screenlets.Screenlet):
         print "drawed"
         
     
-    def idle(self):
-        idle_list_forwards = ["shime1", "shime1a", "shime1b", "shime2", "shime3"]
-        idle_list_backwards = idle_list_forwards[::-1]
-        idle = idle_list_forwards + idle_list_backwards
-        for i in idle:
-            self.draw_idle(i)
-            sleep(1)
-            
-    def on_unfocus (self, event):
-        """Called when the Screenlet's window loses focus."""
-        self.idle()
+#    def idle(self):
+#        idle_list_forwards = ["shime1", "shime1a", "shime1b", "shime2", "shime3"]
+#        idle_list_backwards = idle_list_forwards[::-1]
+#        self.idle_positions = idle_list_forwards + idle_list_backwards
+#        return self.idle_positions
+#            
+#    def on_unfocus (self, event):
+#        """Called when the Screenlet's window loses focus."""
+#        self.idle()
 
 
         
